272e955064161715143896e944065365f074bf20
Kira Zero <longdragon02@gmail.com>
2022-11-09 00:23:12 +0700

    Almost there

--- dynastring.h
+++ dynastring.h
@@ -4,5 +4,46 @@
 #include <stdlib.h>
 #include <string.h>
 
+struct dstrings {
+    char* str;
+};
+typedef struct dstrings String;
+
+String GetStr(String dynammic_string, int length)
+{
+    dynammic_string.str = (char*)malloc(length * sizeof(char));
+    if (dynammic_string.str == NULL)
+    {
+        printf("Malloc failed ! Aborted !\n");
+        return dynammic_string;
+    }
+    //get input
+    fgets(dynammic_string.str, length, stdin);
+    if (dynammic_string.str[0] == '\n')
+        dynammic_string.str = " ";
+
+    strtok(dynammic_string.str, "\n");
+    length = 0;
+    //fine until here
+    char *p = dynammic_string.str;
+    while (*p != NULL)
+    {
+        p++;
+        length++;
+    }
+    //realloc the string and return it
+    dynammic_string.str = realloc(dynammic_string.str, length * sizeof(char));
+    return dynammic_string;
+}
+
+void print(String dynammic_string)
+{
+    printf(dynammic_string.str);
+}
+
+void destroy(String dynammic_string)
+{
+    free(dynammic_string.str);
+}
 
 #endif // DYNASTRING_H
--- main.c
+++ main.c
@@ -3,6 +3,7 @@
 #include <conio.h>
 #include <math.h>
 #include <string.h>
+#include "dynastring.h"
 
 //int to bool
 const char* toBool(int a)
@@ -149,9 +150,9 @@ int notmain3()
     printf("Enter the string here: ");
     fgets(str, l, stdin);
     strtok(str, "\n");
+    l = 0;
     //fine until here
     char *p = str;
-    l = 0;
     while (*p != NULL)
     {
         p++;
@@ -164,12 +165,21 @@ int notmain3()
     return 0;
 }
 
+int notmain4()
+{
+    String s1;
+    s1 = GetStr(s1, 12);
+    print(s1);
+    destroy(s1);
+    return 0;
+}
+
 int main()
 {
     notmain1();
     notmain2();
-    notmain3();
     printf("\n");
     system("PAUSE");
+    notmain4();
     return 0;
 }
